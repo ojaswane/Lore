b5fc95b8417ed286e708e6163275ec36fc04afb7
ojas wane <ojaswane@ojass-MacBook.local>
2026-06-02 13:29:24 +0530

    Updated

--- main.rs
+++ main.rs
@@ -1,5 +1,11 @@
 // Lets start our contribution by creating first a terminal emulator using ratatui
+use ratatui::{DefaultTerminal, Terminal};
+use color-eyre::eyre::Result; // this is the error handling library we will be using
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    color-eyre::install()?;
+
+    // Application code goes here 
+    
+    OK(());
 }
