4930f476ef9fa97ddacae6138dab0d3eea3f5808
Ojas Wane <ojaswane79@gmail.com>
2026-06-03 15:07:21 +0530

    Writing pesudo terminal logic

--- core/shell.rs
+++ core/shell.rs
@@ -1,27 +1,25 @@
-// we will be using an library called protable-pty for the pesudo terminal 
-use::portable_pty::{CommandBuilder, NativePtySystem , PtySize , Child};
-use::color_eyre::eyre::Result; // this is the error handling library we will be using
+// we will be using a library called portable-pty for the pseudo terminal
+use portable_pty::{CommandBuilder, NativePtySystem, PtySize, Child};
+use color_eyre::eyre::Result; // this is the error handling library we will be using
 
 
-pub fn shell() -> Result<()> {
+pub fn shell() -> Result<(Box<dyn portable_pty::MasterPty>, Child)> {
     // Use the native pty implementation for the system
     let pty_system = NativePtySystem::default()?;
 
     // Create a new pty with default size
-    let mut pty = pty_system.openpty(PtySize {
+    let pty = pty_system.openpty(PtySize {
         rows: 24,
         cols: 80,
         pixel_width: 0,
         pixel_height: 0,
     })?;
 
-    // spawinging the shell
+    // spawning the shell
     let cmd = pty.spawn_command(CommandBuilder::new("zsh"))?;
     
-    let mut reader = pty.try_clone_reader()?;
-    let mut writer = pty.try_clone_writer()?;
+    let _reader = pty.try_clone_reader()?;
+    let _writer = pty.try_clone_writer()?;
 
-    
     Ok((pty, cmd))
-    
 }
