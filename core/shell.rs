4a3cbfdea0aa5ae687f37b2f25349718a421d086
Ojas Wane <ojaswane79@gmail.com>
2026-06-03 15:50:56 +0530

    Updated

--- core/shell.rs
+++ core/shell.rs
@@ -1,7 +1,7 @@
 // we will be using a library called portable-pty for the pseudo terminal
 use portable_pty::{CommandBuilder, NativePtySystem, PtySize, Child};
 use color_eyre::eyre::Result; // this is the error handling library we will be using
-
+use std::io::{Read, Write};
 
 pub fn shell() -> Result<(Box<dyn portable_pty::MasterPty>, Child)> {
     // Use the native pty implementation for the system
