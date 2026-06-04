7306d17c66eea79962fd31bb4bc71148e94ea24e
Ojas Wane <ojaswane79@gmail.com>
2026-06-04 17:44:35 +0530

    Debugging

--- core/shell.rs
+++ core/shell.rs
@@ -3,7 +3,8 @@ use portable_pty::{CommandBuilder, NativePtySystem, PtySize, Child, MasterPty , 
 use color_eyre::eyre::Result; // this is the error handling library we will be using
 
 
-pub fn shell() -> Result<(Box<dyn portable_pty::MasterPty>, Child)> {
+// the dyn keyword is used to indicate that the type of the master pty is not known at compile time and it will be determined at runtime
+pub fn shell() -> Result<(Box<dyn portable_pty::MasterPty>, Child)> { 
     // Use the native pty implementation for the system
     let pty_system = NativePtySystem::default();
 
@@ -16,7 +17,8 @@ pub fn shell() -> Result<(Box<dyn portable_pty::MasterPty>, Child)> {
     })?;
 
     // spawning the shell
-    let cmd = pty.slave.spawn_command(CommandBuilder::new("zsh"))?;
+    // this is the slave and the ratatui is the master
+    let cmd = pty.slave.spawn_command(CommandBuilder::new("zsh"))?; 
     
     let _reader = pty.try_clone_reader()?;
     let _writer = pty.try_clone_writer()?;
