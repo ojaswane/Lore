c3fdbd2a31c57c2c33787b3f38b112f8aa864de9
Ojas Wane <ojaswane79@gmail.com>
2026-06-10 23:50:38 +0530

    Updated

--- core/state.rs
+++ core/state.rs
@@ -7,8 +7,13 @@ fn output_shell(
     output: Arc<Mutex<String>>,
 ) {
 
-    // Spawing a new thread for each output
+    // Spawing a new thread for each output .
+    //  we need to spawn a thread because we need to read the output from the shell and print it to the terminal and we don't want to block the main thread while reading the output from the shell
     thread::spawn(move || {
-        
+        let mut buffer = [0; 1024]; //buffer to store the output from the shell
+
+        loop {
+        }
+
     })
 }
