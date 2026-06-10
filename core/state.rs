20550c17bca24860e2d8832b85afc6d36e13a2b4
Ojas Wane <ojaswane79@gmail.com>
2026-06-10 23:35:32 +0530

    Updated

--- core/state.rs
+++ core/state.rs
@@ -1,8 +1,14 @@
 // This file will print the output from shell to the ratatui 
+use std::thread;
+use std::sync::{Arc , Mutex};
 
 fn output_shell(
     mut reader: Box<dyn Read + Send>, // the dyn keyword is to tell that it will be complied at realtime 
     output: Arc<Mutex<String>>,
 ) {
 
+    // Spawing a new thread for each output
+    thread::spawn(move || {
+        
+    })
 }
