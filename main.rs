f9037d0d639dc8e662bb10e5b465c54d2b07f967
Ojas Wane <ojaswane79@gmail.com>
2026-06-11 10:43:10 +0530

    Connected the files

--- main.rs
+++ main.rs
@@ -2,7 +2,8 @@
 use ratatui::DefaultTerminal;
 use crossterm::event::{self , Event , KeyCode}; // this is the library we will be using to handle the events of the terminal
 use anyhow::Result; // this is the error handling library we will be using
-use crate::core::{io::system_io, pty::shell};
+use crate::core::{io::system_io, pty::shell, state::output_shell};
+use std::sync::{Arc, Mutex};
 
 mod ui;
 mod core;
@@ -22,9 +23,10 @@ fn main() -> Result<()> {
 fn app(mut terminal: DefaultTerminal) -> Result<()> {
 
     let (master, _child) = shell()?;
-    let (_reader, _writer) = system_io(master.as_ref())?;
-
+    let output = Arc::new(Mutex::new(String::new()));
     let mut text = String::new();
+    let (reader, _writer) = system_io(master.as_ref())?;
+    let _handle = output_shell(reader, output.clone());
 
     loop{
         // This will draw the Ui to show onto the terminal and show the output as we want
