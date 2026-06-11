93bb11fc948df8a78c1a00d8c5759e72fe7bbdfa
Ojas Wane <ojaswane79@gmail.com>
2026-06-11 10:43:33 +0530

    Updated

--- core/state.rs
+++ core/state.rs
@@ -3,7 +3,6 @@ use std::io::Read;
 use std::thread;
 use std::sync::{Arc , Mutex};
 
-#[allow(dead_code)]
 pub fn output_shell(
     mut reader: Box<dyn Read + Send>, // the dyn keyword is to tell that it will be complied at realtime 
     output: Arc<Mutex<String>>,
