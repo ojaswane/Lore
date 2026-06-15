034a69225260f0e13dc86d0e1c2d6683048cb2c0
Ojas Wane <ojaswane79@gmail.com>
2026-06-15 15:01:45 +0530

    Added Schema

--- db/storage.rs
+++ db/storage.rs
@@ -1,1 +1,14 @@
 // database schemas to store the data at rustql
+
+struct Schema {
+    id: i64,
+    session_id: i64,
+    timestamp: i64,
+    command: String, // this is where the command will be stored
+    dir: String,     // The directory will be stored
+    output: String,
+    error: bool,
+    exit_code: i32,
+    duration_ms: i64, // how long the command took to run , useful for AI context
+    shell: String,    // zsh / bash / fish matters when replaying commands
+}
