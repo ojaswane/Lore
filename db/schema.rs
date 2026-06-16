79cb066bf3fb37c39d568e96dde3cdf4a6dec075
Ojas Wane <ojaswane79@gmail.com>
2026-06-16 11:26:25 +0530

    Connected the db to main file

---
+++ db/schema.rs
@@ -0,0 +1,21 @@
+pub struct Schema {
+    id: i64,
+    session_id: i64,
+    timestamp: i64,
+    command: String, // this is where the command will be stored
+    dir: String,     // The directory will be stored
+    output: String,
+    error: bool,
+    exit_code: i32,
+    duration_ms: i64, // how long the command took to run , useful for AI context
+    shell: String,    // zsh / bash / fish matters when replaying commands
+}
+
+// This is usefull for "show me everything I did last Tuesday in the lore project"
+pub struct Session {
+    id: i64,
+    started_at: i64,     // unix timestamp
+    ended_at: i64,       // so you know session duration
+    project: String,     // inferred from dir — "lore", "hellorust" etc
+    total_commands: i32, // quick stat without counting rows
+}
--- db/storage.rs
+++ db/storage.rs
@@ -1,31 +1,17 @@
-// database schemas to store the data at rustql
+use super::schema;
+use rusqlite::{Connection, Result};
 
-struct Schema {
-    id: i64,
-    session_id: i64,
-    timestamp: i64,
-    command: String, // this is where the command will be stored
-    dir: String,     // The directory will be stored
-    output: String,
-    error: bool,
-    exit_code: i32,
-    duration_ms: i64, // how long the command took to run , useful for AI context
-    shell: String,    // zsh / bash / fish matters when replaying commands
+// to init the db
+pub fn init_db() -> Result<()> {
+    // initialising the table
+    let lore = Connection::open("lore.db");
 }
 
-// This is usefull for "show me everything I did last Tuesday in the lore project"
-struct Session {
-    id: i64,
-    started_at: i64,     // unix timestamp
-    ended_at: i64,       // so you know session duration
-    project: String,     // inferred from dir — "lore", "hellorust" etc
-    total_commands: i32, // quick stat without counting rows
-}
-
-pub fn init_db() {}
-
+// start the session
 pub fn session_init() {}
 
+// save the commands
 pub fn save_command() {}
 
+// end the session
 pub fn end_session() {}
