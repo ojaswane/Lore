4699e28996ae5835ee381db11fa302d1656f6eb8
Ojas Wane <ojaswane79@gmail.com>
2026-08-28 15:31:00 +0530

    feat: implement chunking output in ingest worker and log chunks

--- core/ingest.rs
+++ core/ingest.rs
@@ -50,6 +50,12 @@ pub fn ingest_worker(rx: std::sync::mpsc::Receiver<IngestEvent>) {
                 exit_code,
                 duration_ms,
             } => {
+                // calls the chunking function to split the output into smaller chunks
+                let chunks = crate::core::chunk::chunk_output(&output);
+
+                // TODO , SAVE IT TO DIFFERENT TABLE IN DB FOR EMBEDDINGS
+                println!("Chunks: {:?}", chunks);
+
                 // Insert the command into the database
                 if let Err(e) = crate::db::storage::save_command(
                     &conn,
